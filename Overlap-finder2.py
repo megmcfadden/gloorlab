93a49da427330b1d3d13bfeec5a3ea17790a7ea3
megmcfadden <mmcfad5@uwo.ca>
2021-07-02 15:57:23 -0400

    /

--- Overlap-finder2.py
+++ Overlap-finder2.py
@@ -5,25 +5,5 @@ import os, os.path
 import sys
 import pandas as pd
 
-pairs= 'tene_sub_filenames.txt'
+pairs= 'tene_sub_filenames.txt')
 print(pairs)
-
-'''hmer_ext = ['*.txt']
-orf_ext = ['*4orf.gff']
-
-
-path = sys.argv[1]
-outname= sys.argv[2]
-
-files_hmer = [h for h_ext in hmer_ext for h in glob.glob(os.path.join(path, h_ext))]
-files_orf = [o for o_ext in orf_ext for o in glob.glob(os.path.join(path, o_ext))]
-#"{}_overlap.txt".format(files_hmer) #this is a guess - not sure if it will work
-pairs = []
-
-for file in files_hmer:
-    file_split = file.replace('Hmer_', '.txt').split('.txt')
-    match= [file for file in files_orf if file_split[1] in file]
-    pairs.append([file, match[0]])
-
-output= path + "/" + outname
-find_overlap(pairs,output)'''
