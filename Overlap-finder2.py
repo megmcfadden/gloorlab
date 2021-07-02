c9958d43607434c4e01b65292f8b2242e1ead9bf
megmcfadden <mmcfad5@uwo.ca>
2021-07-02 18:33:47 -0400

    .

--- Overlap-finder2.py
+++ Overlap-finder2.py
@@ -1,4 +1,4 @@
-from overlap2 import find_overlap
+from overlap import find_overlap
 
 import glob
 import os, os.path
@@ -6,15 +6,22 @@ import sys
 import pandas as pd
 
 hmer= pd.read_table('tene_sub_filenames_in.txt',names=['hmer'])
-gff=pd.read_table('tene_sub_filenames_out.txt')
-
-hmer['gff']=gff
+gff=pd.read_table('tene_sub_filenames_out.txt', names=['gff'])
 
 
-pairs=hmer[['hmer','gff']].apply(tuple,axis=1).tolist()
+hmer2=hmer['hmer'].tolist()
+gff2=gff['gff'].tolist()
+print(type(hmer2))
+pairs=[]
 
+for file in hmer2:
+    print(file)
+    file_split = file.replace('Hmer_', '.txt').split('.txt')
+    print(file_split[1])
+    match= [file for file in gff2 if file_split[1] in file]
+    pairs.append([file, match[0]])
 
 print(pairs)
 
 
-#find_hmer(pairs,'/Volumes/ubdata/mmcfad/NCBI_Genomes/Output_files/July_2_sub.txt')
+find_hmer(pairs,'/Volumes/ubdata/mmcfad/NCBI_Genomes/Output_files/July_2_sub.txt')
