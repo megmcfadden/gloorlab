eea7ad50816e61ccbd8db4ce0c7afbf48e28dd9e
megmcfadden <mmcfad5@uwo.ca>
2021-07-26 10:09:49 -0400

    c

--- split_lin.sh
+++ split_lin.sh
@@ -1,7 +1,6 @@
-for fasta in $(ls /Volumes/ubdata/mmcfad/NCBI_Genomes/*.fna);do
-  awk '/^>/ {if(N>0) printf("\n"); printf("%s\n",$0);++N;next;} {printf("%s",$0):} END {printif("\n");}' |\
+cat /Volumes/ubdata/mmcfad/NCBI_Genomes/*.fna |\
+  awk '/^>/ {if(N>0) printf("\n"); printf("%s\n",$0);++N;next;} { printf("%s",$0);} END {printf("\n");}' |\
   split -l 2 --additional-suffix=single.fna - seq_
-done
 
 
 #for file in OUT*
