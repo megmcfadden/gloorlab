defe138a793c4fc831f57fbf6bee8a85f9d3c2f6
megmcfadden <mmcfad5@uwo.ca>
2021-07-26 11:54:37 -0400

    a

--- Hmer_ORF_Overlap_Shell.sh
+++ Hmer_ORF_Overlap_Shell.sh
@@ -1,16 +1,16 @@
 
 #Run prodigal to get the open reading frames
-for fasta in $(ls /Volumes/ubdata/mmcfad/NCBI_Genomes/*.fna); do
-output=$(python3 prodigal_filenames.py $fasta)
-echo $fasta
-echo $output
-prodigal -i $fasta -o $output -a protein-translations.faa -f gff
-done
+#for fasta in $(ls /Volumes/ubdata/mmcfad/NCBI_Genomes/*.fna); do
+#output=$(python3 prodigal_filenames.py $fasta)
+#echo $fasta
+#echo $output
+#prodigal -i $fasta -o $output -a protein-translations.faa -f gff
+#done
 
 #Execute HMER_Finder script
-#path=/Volumes/ubdata/mmcfad/NCBI_Genomes
+path=/Volumes/ubdata/mmcfad/NCBI_Genomes
 #path=/home/meg/Documents/project
-#python3 Hmer-finder.py $path
+python3 Hmer-finder.py $path
 
 #Execute Overlap-finder
 #outputname="Output_files/July_2_ALL.txt"
