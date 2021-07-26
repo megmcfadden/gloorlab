07b9e559f2e18b55a791f074e6567b4483a3bb32
megmcfadden <mmcfad5@uwo.ca>
2021-07-26 10:55:51 -0400

    x

--- split_lin.sh
+++ split_lin.sh
@@ -5,7 +5,7 @@ echo $output
 done
 
 cat /Volumes/ubdata/mmcfad/NCBI_Genomes/*.fna |\
-awk '/^>/ {if(N>0) printf("\n"); printf("%s\n",$0);++N;next;} { printf("%s",$0);} END {printf("\n");}' | $\
+awk '/^>/ {if(N>0) printf("\n"); printf("%s\n",$0);++N;next;} { printf("%s",$0);} END {printf("\n");}' | \
 split -l 2 -$output
 
 
