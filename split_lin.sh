8a5196901dc44c52a44a65e1ce74b804f643ddcf
megmcfadden <mmcfad5@uwo.ca>
2021-07-26 10:41:05 -0400

    c

--- split_lin.sh
+++ split_lin.sh
@@ -2,8 +2,8 @@ for fasta in $(ls /Volumes/ubdata/mmcfad/NCBI_Genomes/*.fna); do
 output=$(python3 hmer_filenames.py $fasta)
 echo $fasta
 echo $output
-awk '/^>/ {if(N>0) printf("\n"); printf("%s\n",$0);++N;next;} { printf("%s",$0);} END {printf("\n");}' | $fasta
-split -l 2 -$output
+echo awk '/^>/ {if(N>0) printf("\n"); printf("%s\n",$0);++N;next;} { printf("%s",$0);} END {printf("\n");}' | $fasta
+echo split -l 2 -$output
 done
 
 
