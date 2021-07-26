9d281e4b97092f98bd0d921fc07f7343df01f58b
megmcfadden <mmcfad5@uwo.ca>
2021-07-26 09:56:11 -0400

    executeable

--- split_lin.sh
+++ split_lin.sh
@@ -1,3 +1,5 @@
+#!bin/#!/usr/bin/env bash
+
 for fasta in $(ls /Volumes/ubdata/mmcfad/NCBI_Genomes/*.fna);do
   awk '/^>/ {if(N>0) printf("\n"); printf("%s\n",$0);++N;next;} {printf("%s",$0):} END {printif("\n");}' |\
   split -1 2 - OUT
